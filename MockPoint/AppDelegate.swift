9b6ee0f777e6df5c120ba036d2a27a35aa23cd64
Peter Sepstrup <peter@sepstrupnet.dk>
2017-10-25 16:46:28 +0200

    Initial Commit

---
+++ MockPoint/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MockPoint
+//
+//  Created by Peter Sepstrup on 25/10/17.
+//  Copyright © 2017 powerLABS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MockPoint/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MockPoint
+//
+//  Created by Peter Sepstrup on 25/10/17.
+//  Copyright © 2017 powerLABS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
